852cae30d4fe46ef70d04565a2894e1d7d35be8f
Edson <edsonsjesus@gmail.com>
2020-06-15 23:25:36 -0300

    Refactoring and improved feedback to user.

--- hangman.py
+++ hangman.py
@@ -4,24 +4,39 @@ def jogar():
     print("***    Welcome to Hangman!    ***")
     print("*********************************\n")
 
-    secret_word = "banana"
-    user_word = ['_', '_', '_', '_', '_', '_']
-    hit = False
-    hanged = False
+    secret_word = "banana".upper()
+    user_word = ['_' for l in secret_word]
+    errors = 0
+    attempts = 6
 
     print('Word: {}\n'.format(user_word))
-    while(not hit and not hanged):
+
+    while True:
         answer = input("Type a letter: ")
-        answer = answer.strip()
-        index = 0
-        for l in secret_word.lower():
-            if l == answer.lower():
-                user_word[index] = answer
-            index = index+1
-        print('Your word: {}\n'.format(user_word))
+        answer = answer.strip().upper()
+
+        if answer in user_word:
+            print('You already typed it! Try again!\n')
+            continue
+
+        if answer in secret_word:
+            index = 0
+            for l in secret_word:
+                if l == answer:
+                    user_word[index] = answer
+                index += 1
+            print('Yep!! Your word: {}\n'.format(user_word))
+        else:
+            errors += 1
+            print('You missed! There are {} attempts left.'.format(attempts-errors))
+
+        if errors == attempts:
+            print("\n***** You lost! Try again! ******")
+            break
+
         if '_' not in user_word:
-            hit = True
             print("*** Congratulations! You win! ***")
+            break
     print("********** Game over! ***********")
 
 if (__name__ == "__main__"):
